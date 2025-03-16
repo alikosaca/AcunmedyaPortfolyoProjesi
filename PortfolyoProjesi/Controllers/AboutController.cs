75f15c960b625c137a4b96e57c660d39b7886742
ali <137689491+alikosaca@users.noreply.github.com>
2025-03-16 16:44:29 +0300

    mig_About_Get_And_Post

--- PortfolyoProjesi/Controllers/AboutController.cs
+++ PortfolyoProjesi/Controllers/AboutController.cs
@@ -10,11 +10,28 @@ namespace PortfolyoProjesi.Controllers
     public class AboutController : Controller
     {
         // GET: About
-        PortfolioAcunMediaEntities1 portfolioAcunMediaEntities = new PortfolioAcunMediaEntities1();
+        PortfolioAcunMediaEntities1 aboutManager = new PortfolioAcunMediaEntities1();
+        [HttpGet]
         public ActionResult Index()
         {
-            var values = portfolioAcunMediaEntities.Tbl_About.ToList();
+            var values = aboutManager.Tbl_About.Find(1);
             return View(values);
+        }
+        [HttpPost]
+        public ActionResult Index(Tbl_About _About)
+        {
+            var values = aboutManager.Tbl_About.Find(1);
+            values.Title = _About.Title;
+            values.Description = _About.Description;
+            values.Description1 = _About.Description1;
+            values.Age = _About.Age;
+            values.Website = _About.Website;
+            values.Phone = _About.Phone;
+            values.City = _About.City;
+            values.Email = _About.Email;
+            values.Degree = _About.Degree;
+            aboutManager.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
