1c4d3e9818c05a51e9371a634ddc3fb5f75e9afb
ali <137689491+alikosaca@users.noreply.github.com>
2025-03-19 07:25:27 +0300

    mig_add_Sider

--- PortfolyoProjesi/Controllers/SiderController.cs
+++ PortfolyoProjesi/Controllers/SiderController.cs
@@ -3,15 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using PortfolyoProjesi.Models;
 namespace PortfolyoProjesi.Controllers
 {
     public class SiderController : Controller
     {
-        // GET: Sider
+        PortfolioAcunMediaEntities1 db = new PortfolioAcunMediaEntities1();
         public ActionResult Index()
         {
-            return View();
+            var values = db.Tbl_Sider.Find(1);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult Index(Tbl_Sider _Sider)
+        {
+            var sider = db.Tbl_Sider.FirstOrDefault();
+            if (sider != null)
+            {
+                sider.Title = _Sider.Title;
+                sider.Description = _Sider.Description;
+                sider.ImageUrl = _Sider.ImageUrl;
+                db.SaveChanges();
+            }
+            else
+            {
+                db.Tbl_Sider.Add(_Sider);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
--- PortfolyoProjesi/Controllers/SkillController.cs
+++ PortfolyoProjesi/Controllers/SkillController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PortfolyoProjesi.Models;
 
 namespace PortfolyoProjesi.Controllers
 {
     public class SkillController : Controller
     {
+        PortfolioAcunMediaEntities1 db = new PortfolioAcunMediaEntities1();
         // GET: Skill
         public ActionResult Index()
         {
