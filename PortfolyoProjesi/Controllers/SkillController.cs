6242d14830f089b4cbb2f0142d71e156b5cfca75
ali <137689491+alikosaca@users.noreply.github.com>
2025-03-19 08:05:24 +0300

    mig_add_skill

--- PortfolyoProjesi/Controllers/SkillController.cs
+++ PortfolyoProjesi/Controllers/SkillController.cs
@@ -10,10 +10,49 @@ namespace PortfolyoProjesi.Controllers
     public class SkillController : Controller
     {
         PortfolioAcunMediaEntities1 db = new PortfolioAcunMediaEntities1();
-        // GET: Skill
         public ActionResult Index()
         {
+            var values = db.Tbl_Skill.ToList();
+            return View(values);
+        }
+        public ActionResult AddSkill()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddSkill(Tbl_Skill _Skill)
+        {
+            db.Tbl_Skill.Add(_Skill);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult EditSkill(int id)
+        {
+            var values = db.Tbl_Skill.Find(id);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult EditSkill(Tbl_Skill _skill)
+        {
+            var values = db.Tbl_Skill.Find(_skill.SkillID);
+            if (_skill != null)
+            {
+                values.SkillName = _skill.SkillName;
+                values.Description = _skill.Description;
+                db.SaveChanges();
+            }
+            else
+            {
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteSkill(int id)
+        {
+            var values = db.Tbl_Skill.Find(id);
+            db.Tbl_Skill.Remove(values);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
